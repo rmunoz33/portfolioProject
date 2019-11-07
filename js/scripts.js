90cb893e7cd10ca763973c7536d91be126af7e70
Rocky Munoz <rmunoz@Rocky-Munoz-Macbook-Pro-Retina.local>
2019-11-07 13:31:45 -0700

    add button styling and hover changes with event listeners

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,43 @@
-$(function() {
-    $(".carousel").carousel( { interval: 2000 } );
-    $("#carouselButton").click(function(){
-        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
-            $(".carousel").carousel("pause");
-            $("#carouselButton").children("i").removeClass("fa-pause");
-            $("#carouselButton").children("i").addClass("fa-play");
-        } else {
-            $(".carousel").carousel("cycle");
-            $("#carouselButton").children("i").removeClass("fa-play");
-            $("#carouselButton").children("i").addClass("fa-pause"); 
-        }
-    });
-});
+$(".choice").on("click", function() {
+    console.log("test");
+    $(".choice").removeClass("expand unset overflow overflow-auto");
+    $(".choice").addClass("small");
+    $(this).removeClass("small");
+    $(this).addClass("expand overflow overflow-auto");
+})
+
+
+const elJumbo = document.querySelector(".reload");
+elJumbo.addEventListener('click', backToHome);
+
+function backToHome() {
+    window.location.reload(true);
+}
+
+const elButton = document.querySelector(".loginButton");
+elButton.addEventListener('mouseover', hoverBgColor);
+elButton.addEventListener('mouseout', unhoverBgColor);
+
+function hoverBgColor() {
+    elButton.style.color = 'white';
+    elButton.style.background = 'black';
+}
+
+function unhoverBgColor() {
+    elButton.style.color = 'black';
+    elButton.style.background = '#1dff00';
+}
+
+const elCardButton = document.querySelector(".cardButton");
+elCardButton.addEventListener('mouseover', cardHoverBgColor);
+elCardButton.addEventListener('mouseout', cardUnhoverBgColor);
+
+function cardHoverBgColor() {
+    elCardButton.style.color = 'black';
+    elCardButton.style.background = 'white';
+}
+
+function cardUnhoverBgColor() {
+    elCardButton.style.color = 'white';
+    elCardButton.style.background = 'black';
+}
